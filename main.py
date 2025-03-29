6a52478afea7eb883820803ca14e601e193291f4
Partheo <therarpar@gmail.com>
2025-03-28 23:40:27 -0400

    add movable player sprite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 
 def main():
@@ -10,14 +11,29 @@ def main():
     pygame.init()
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
+    player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
 
     while(True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        
+            
+        updatable.update(dt)
+
         screen.fill((0,0,0))
+        for entity in drawable:
+            entity.draw(screen)
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
